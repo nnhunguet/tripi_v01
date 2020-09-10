ab75ee4f8b1b9e9792169dd14f2b3242367bef3b
Truong216 <tn12345678910truong@gmail.com>
2020-09-11 00:24:22 +0700

    style hotel_find

---
+++ components/Data.js
@@ -0,0 +1,52 @@
+export const DATA = [
+    {
+        id: '1',
+        title: 'Hà Nội',
+        link: require('../assets/Hanoi.jpg')
+    },
+    {
+        id: '2',
+        title: 'Hồ Chí Minh',
+        link: require('../assets/HoChiMinh.jpg')
+    },
+    {
+        id: '3',
+        title: 'Đà Nẵng',
+        link: require('../assets/Danang.jpg')
+    },
+    {
+        id: '4',
+        title: 'Bà Rịa - Vũng Tàu',
+        link: require('../assets/Vungtau.jpg')
+    },
+    {
+        id: '5',
+        title: 'Đà Lạt',
+        link: require('../assets/Dalat.jpg')
+    },
+    {
+        id: '6',
+        title: 'Khánh Hòa',
+        link: require('../assets/Welcome.jpg')
+    },
+    {
+        id: '7',
+        title: 'Quảng Nam',
+        link: require('../assets/Welcome.jpg')
+    },
+    {
+        id: '8',
+        title: 'Quảng Ninh',
+        link: require('../assets/Welcome.jpg')
+    },
+    {
+        id: '9',
+        title: 'Kiên Giang',
+        link: require('../assets/Welcome.jpg')
+    },
+    {
+        id: '10',
+        title: 'Bình Thuận',
+        link: require('../assets/Welcome.jpg')
+    },
+];
