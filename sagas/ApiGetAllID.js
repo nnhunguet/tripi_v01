f8a0e07ced1278a4b3dd9e6e0d35f1277ece583a
nnhunguet <52577080+nnhunguet@users.noreply.github.com>
2020-09-11 05:28:33 +0700

    Get All Price - Done

---
+++ sagas/ApiGetAllID.js
@@ -0,0 +1,23 @@
+const urlGetAllID = 'http://13.250.18.254:5000/hotels/getAllId';
+import axios from 'axios';
+
+function* getAllID(id) {
+  let response;
+  yield axios.get(`${urlGetAllID}/${id}`)
+  .then(function (res) {
+    // handle success
+    response = res.data;
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    return response;
+  });
+  return response;
+}
+
+export const Api2 = {
+  getAllID
+}
--- sagas/getInforHotelSaga.js
+++ sagas/getInforHotelSaga.js
@@ -1,4 +1,6 @@
 import { Api }  from './ApiGetInforHotel';
+import { Api2 } from './ApiGetAllID';
+import { Api3 } from './ApiGetAllPrice';
 import { GET_INFOR_HOTEL, GET_INFOR_HOTEL_SUCCEEDED, GET_INFOR_HOTEL_FAILD } from '../redux/actionTypes';
 
 import { put, takeLatest } from 'redux-saga/effects';
@@ -10,6 +12,15 @@ function* getInforHotel({ payload }) {
     yield put(showLoading());
     const inforHotel = yield (Api.getInforHotel(hotel_id));
     console.log('getInforHotel - Saga', inforHotel);
+    const getAllID = yield (Api2.getAllID(hotel_id));
+    // console.log(getAllID);
+    let arrDomian = [];
+    for(let item of getAllID) {
+      arrDomian.push(`${item.domain_id}_${item.domain_hotel_id}_1232131`);
+    }
+    console.log(arrDomian);
+    const getAllPrice = yield (Api3.getAllPrice(arrDomian));
+    console.log('all price',getAllPrice);
     yield put({ type: GET_INFOR_HOTEL_SUCCEEDED, payload: { data: inforHotel } });  
   } 
   catch(err) {
